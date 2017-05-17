feeb6d222e8bd958e7a6252979d85dd978df74fd
Mansiper <mansiper@yandex.ru>
2017-05-17 20:47:13 +0500

    Version 1.4
    New action. Open post in background.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Hotkeys for news feed in VK (VKontakte). Userscript.
 + k - backward
 + l - load new (on top)
 + m - load old (at the bottom)
++ n - open post in background
 + p - block repost source (you will not sea news out of there again)
 
 ---
@@ -13,4 +14,5 @@ Hotkeys for news feed in VK (VKontakte). Userscript.
 + k - назад
 + l - загрузить новые (вверху страницы)
 + m - загрузить старые (внизу страницы)
++ n - открыть новость в фоне
 + p - заблокировать источник репоста (больше не увидите новости оттуда)
--- vk_jk.user.js
+++ vk_jk.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name vk_jk
-// @description Hotkeys for news feed in VK (j - forward, k - backward, l - load new, m - load old, p - block repost)
+// @description Hotkeys for news feed in VK (https://github.com/Mansiper/vk_jk)
 // @author Mansiper
 // @license MIT
-// @version 1.2
+// @version 1.4
 // @include https://vk.com/*
 // ==/UserScript==
 (function (window, undefined) {  
@@ -43,6 +43,10 @@
 		
 		var loopMove = function(post) {
 			if (post == undefined) return false;
+			if (post.id == '') {
+				post = post.firstChild;
+				if (post == undefined || post.id == '') return false;
+			}
 			var hasAdv = post.id.startsWith('ads_');
 			if (!hasAdv) {
 				var classes = post.classList;
@@ -77,6 +81,17 @@
 		} else if (code == 77/*m*/) {
 			var btn = document.getElementById('show_more_link');
 			btn.click();
+		} else if (code == 78/*n*/) {
+			if (curObjId == '') return;
+			var post = document.getElementById(curObjId);
+			var link = post.getElementsByClassName('post_link')[0].href;
+			//thanks to http://stackoverflow.com/a/11389138
+			var a = document.createElement("a");
+			a.href = link;
+			var evt = document.createEvent("MouseEvents");
+			evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
+																	true, false, false, false, 0, null);
+			a.dispatchEvent(evt);
 		} else if (code == 80/*p*/) {
 			if (curObjId == '') return;
 			var post = document.getElementById(curObjId);
